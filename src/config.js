a0785216dff9cf84e34925b678dac70a32495733
Allan <epicallan.al@gmail.com>
2016-01-21 10:07:48 +0300

    added google tracking

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
 export const port = process.env.PORT || 5000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
-export const googleAnalyticsId = 'UA-XXXXX-X';
+export const googleAnalyticsId = 'UA-72716212-1';
 export const MONGO_URL = 'mongodb://localhost/mine';
